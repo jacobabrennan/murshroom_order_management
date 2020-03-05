a68dc1435495c37c216111c27a27875cdcd7733b
JacobABrennan <jacobabrennan@gmail.com>
2020-03-05 17:57:11 -0500

    Create client/server boilerplate

---
+++ client/src/index.js
@@ -0,0 +1,11 @@
+
+
+//==============================================================================
+
+//-- Dependencies --------------------------------
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './reset.css';
+
+//------------------------------------------------
+ReactDOM.render(<React.Fragment />, document.getElementById('root'));
